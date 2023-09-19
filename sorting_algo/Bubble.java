b267f0f223b551b162b6b24ba3fa2c6380200340
HEMRAJ KUMAR V S <88047630+Hemraj-kumar@users.noreply.github.com>
2023-09-19 20:07:39 +0530

    Add files via upload

---
+++ sorting_algo/Bubble.java
@@ -0,0 +1,16 @@
+public class Bubble {
+    public static void main(String[] args) {
+        int n=5;
+        int[]arr={45,56,89,90,9};
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<n;i++) System.out.println(arr[i]);
+    }
+}
